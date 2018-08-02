7b42305d4b4a87802fa5df750cf5f0050f3f7c50
Tom Foster <thomasfoster@warwickshire.gov.uk>
2018-08-02 22:53:33 +0100

    python file to read all videos, then split them into images now created for each file, unsure of how many images it will leave me with in the total. Possily around 270,500 images for the year

--- all_video_files.py
+++ all_video_files.py
@@ -23,12 +23,14 @@ for v in range(len(video_only)):
     print(video_only[v])
     v += 1
 
+# Lup files are your location lat/long in dec minutes
 lup_less_files = list(lup_files)
 for x in range(len(lup_less_files)):
     lup_less_files[x] = lup_less_files[x][0:-4]
     x += 1
 lup_less_files.sort()
 
+# Avi is the video file itself
 avi_less_files = list(video_only)
 for x in range(len(avi_less_files)):
     avi_less_files[x] = avi_less_files[x][0:-4]
@@ -43,3 +45,43 @@ for x in range(len(lup_less_files)):
         print('not matching')
 print(avi_less_files[0], lup_less_files[0])
 print('matching files: ' + str(matching_files))
+
+
+# For each file that matches every frame then has to match with the coordinate
+# match the location to the reference of each frame.
+
+# for every video found produce a image
+import cv2
+import numpy as numpy
+import os
+
+try:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+except OSError:
+    print('Error: Creating directory of data')
+    
+def video_to_images(video_path):
+    video = cv2.VideoCapture(video_path)
+    currentFrame = 1
+    frame_exists = True
+    while(frame_exists):
+        # Capture each frame
+        frame_bool, frame = video.read()
+        if frame_bool == False:
+            frame_exists = False
+        else:
+            # 41 is the absolute path
+            name = './data/' + str(video_path[41:-4] + '_frame_' + str(currentFrame) + '.jpg')
+            print('Creating...' + name)
+            cv2.imwrite(name, frame)
+            currentFrame += 1
+
+    video.release()
+    cv2.destroyAllWindows()
+        
+# video_to_images('A38_NB_YR2_R09_180519132259.avi')
+# video_to_images('../SCANNER 2018 VIDEO/70093 WARWICKSHIRE/' + 'C166_NB_RAV_YR2_R07_180514152253.avi')
+for x in range(len(video_only)):
+    video_to_images('../SCANNER 2018 VIDEO/70093 WARWICKSHIRE/' + video_only[x])
+#     print(video_only[x])
